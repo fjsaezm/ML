66fcd727bf809517b649d1ed113099ff499de21e
Javier Sáez <fjaviersaezm@gmail.com>
2021-03-17 15:48:04 +0100

    Add P1 and changes

---
+++ P1/P1.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+"""
+TRABAJO 1. 
+Francisco Javier Sáez Maldonado
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from math import exp,pow,sqrt
+
+np.random.seed(1)
+
+print('EJERCICIO SOBRE LA BUSQUEDA ITERATIVA DE OPTIMOS\n')
+print('Ejercicio 1\n')
+
+def E(u,v):
+    return pow(u*u*u*exp(v-2) -2*v*v*exp(-u),2)   
+
+#Derivada parcial de E con respecto a u
+def dEu(u,v):
+	return 2*(u*u*u *exp(v-2) -2*exp(-u)*v*v)*(3*u*u*exp(v-2)+2*exp(-u)*v*v)
+    #return 2*sqrt(E(u,v))*(3*u*u*exp(v-2) + 2*v*v*exp(-u))
+    
+#Derivada parcial de E con respecto a v
+def dEv(u,v):
+    return 2*(u*u*u*exp(v-2)-4*exp(-u)*v)*(u*u*u*exp(v-2)-2*exp(u)*v*v)
+	#return 2*sqrt(E(u,v))*(3*u*u*exp(v-2)- 4*v*exp(-u))
+
+#Gradiente de E
+def gradE(u,v):
+    return np.array([dEu(u,v), dEv(u,v)])
+
+def gradient_descent(eta,maxIter,error2get,initial_point):
+	iterations = 0
+	w_t = initial_point
+	for i in range(maxIter):
+		# Compute gradient in the current point
+		g_t = gradE(w_t[0],w_t[1])
+		# Get the opposite direction of the gradient
+		v_t = -g_t
+		# Compute new values
+		w_t = w_t + eta*v_t
+		# Sum iterations and check conditions
+		iterations += 1
+
+		if iterations / 10000000 == 0:
+			print("Iteration ",iterations)
+		if E(w_t[0],w_t[1]) < error2get:
+			break
+    
+	return w_t, iterations
+
+
+eta = 0.1 
+maxIter = 10000000000
+error2get = 1e-14
+initial_point = np.array([1.0,1.0])
+w, it = gradient_descent(eta,maxIter,error2get,initial_point)
+
+
+print ('Numero de iteraciones: ', it)
+print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
+
+
+# DISPLAY FIGURE
+from mpl_toolkits.mplot3d import Axes3D
+x = np.linspace(-30, 30, 50)
+y = np.linspace(-30, 30, 50)
+X, Y = np.meshgrid(x, y)
+Z = E(X, Y) #E_w([X, Y])
+fig = plt.figure()
+ax = Axes3D(fig)
+surf = ax.plot_surface(X, Y, Z, edgecolor='none', rstride=1,
+                        cstride=1, cmap='jet')
+min_point = np.array([w[0],w[1]])
+min_point_ = min_point[:, np.newaxis]
+ax.plot(min_point_[0], min_point_[1], E(min_point_[0], min_point_[1]), 'r*', markersize=10)
+ax.set(title='Ejercicio 1.2. Función sobre la que se calcula el descenso de gradiente')
+ax.set_xlabel('u')
+ax.set_ylabel('v')
+ax.set_zlabel('E(u,v)')
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+#Seguir haciendo el ejercicio...
+
+exit()
+
+
+
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+print('EJERCICIO SOBRE REGRESION LINEAL\n')
+print('Ejercicio 1\n')
+
+label5 = 1
+label1 = -1
+
+# Funcion para leer los datos
+def readData(file_x, file_y):
+	# Leemos los ficheros	
+	datax = np.load(file_x)
+	datay = np.load(file_y)
+	y = []
+	x = []	
+	# Solo guardamos los datos cuya clase sea la 1 o la 5
+	for i in range(0,datay.size):
+		if datay[i] == 5 or datay[i] == 1:
+			if datay[i] == 5:
+				y.append(label5)
+			else:
+				y.append(label1)
+			x.append(np.array([1, datax[i][0], datax[i][1]]))
+			
+	x = np.array(x, np.float64)
+	y = np.array(y, np.float64)
+	
+	return x, y
+
+# Funcion para calcular el error
+def Err(x,y,w):
+    return 
+
+# Gradiente Descendente Estocastico
+#def sgd(?):
+#    #
+#    return w
+
+# Pseudoinversa	
+#def pseudoinverse(?):
+#    #
+#    return w
+
+
+# Lectura de los datos de entrenamiento
+x, y = readData('datos/X_train.npy', 'datos/y_train.npy')
+# Lectura de los datos para el test
+x_test, y_test = readData('datos/X_test.npy', 'datos/y_test.npy')
+
+
+#w = sgd(?)
+print ('Bondad del resultado para grad. descendente estocastico:\n')
+print ("Ein: ", Err(x,y,w))
+print ("Eout: ", Err(x_test, y_test, w))
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+#Seguir haciendo el ejercicio...
+
+print('Ejercicio 2\n')
+# Simula datos en un cuadrado [-size,size]x[-size,size]
+def simula_unif(N, d, size):
+	return np.random.uniform(-size,size,(N,d))
+
+def sign(x):
+	if x >= 0:
+		return 1
+	return -1
+
+def f(x1, x2):
+	#return sign(?) 
+	return 0
+#Seguir haciendo el ejercicio...
