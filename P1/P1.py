286556283e894336945615c3787ff30d39837bd0
Javier Sáez <fjaviersaezm@gmail.com>
2021-03-17 21:51:32 +0100

    Advance P1

--- P1/P1.py
+++ P1/P1.py
@@ -6,83 +6,147 @@ Francisco Javier Sáez Maldonado
 
 import numpy as np
 import matplotlib.pyplot as plt
-from math import exp,pow,sqrt
+import pandas as pd
 
 np.random.seed(1)
 
-print('EJERCICIO SOBRE LA BUSQUEDA ITERATIVA DE OPTIMOS\n')
-print('Ejercicio 1\n')
+# Auxiliar functions to help with the exercises
 
-def E(u,v):
-    return pow(u*u*u*exp(v-2) -2*v*v*exp(-u),2)   
+def wait():
+	input("\n--- Pulsar tecla para continuar ---\n")
 
-#Derivada parcial de E con respecto a u
-def dEu(u,v):
-	return 2*(u*u*u *exp(v-2) -2*exp(-u)*v*v)*(3*u*u*exp(v-2)+2*exp(-u)*v*v)
-    #return 2*sqrt(E(u,v))*(3*u*u*exp(v-2) + 2*v*v*exp(-u))
-    
-#Derivada parcial de E con respecto a v
-def dEv(u,v):
-    return 2*(u*u*u*exp(v-2)-4*exp(-u)*v)*(u*u*u*exp(v-2)-2*exp(u)*v*v)
-	#return 2*sqrt(E(u,v))*(3*u*u*exp(v-2)- 4*v*exp(-u))
+def to_numpy(func):
+  """Decorador para convertir funciones a versión NumPy"""
 
-#Gradiente de E
-def gradE(u,v):
-    return np.array([dEu(u,v), dEv(u,v)])
+  def numpy_func(w):
+    return func(*w)
 
-def gradient_descent(eta,maxIter,error2get,initial_point):
-	iterations = 0
-	w_t = initial_point
-	for i in range(maxIter):
-		# Compute gradient in the current point
-		g_t = gradE(w_t[0],w_t[1])
-		# Get the opposite direction of the gradient
-		v_t = -g_t
-		# Compute new values
-		w_t = w_t + eta*v_t
-		# Sum iterations and check conditions
-		iterations += 1
+  return numpy_func
 
-		if iterations / 10000000 == 0:
-			print("Iteration ",iterations)
-		if E(w_t[0],w_t[1]) < error2get:
-			break
-    
-	return w_t, iterations
-
-
-eta = 0.1 
-maxIter = 10000000000
-error2get = 1e-14
-initial_point = np.array([1.0,1.0])
-w, it = gradient_descent(eta,maxIter,error2get,initial_point)
-
-
-print ('Numero de iteraciones: ', it)
-print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
-
+def print_output_e1(str_f,initial_point,eta,it,w):
+	print("Gradiente descendente sobre la función: " +  str_f)
+	print("Punto inicial: {}".format(initial_point))
+	print("Tasa de aprendizaje: {}".format(eta))
+	print ('Numero de iteraciones: ', it)
+	print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
 
 # DISPLAY FIGURE
 from mpl_toolkits.mplot3d import Axes3D
-x = np.linspace(-30, 30, 50)
-y = np.linspace(-30, 30, 50)
-X, Y = np.meshgrid(x, y)
-Z = E(X, Y) #E_w([X, Y])
-fig = plt.figure()
-ax = Axes3D(fig)
-surf = ax.plot_surface(X, Y, Z, edgecolor='none', rstride=1,
-                        cstride=1, cmap='jet')
-min_point = np.array([w[0],w[1]])
-min_point_ = min_point[:, np.newaxis]
-ax.plot(min_point_[0], min_point_[1], E(min_point_[0], min_point_[1]), 'r*', markersize=10)
-ax.set(title='Ejercicio 1.2. Función sobre la que se calcula el descenso de gradiente')
-ax.set_xlabel('u')
-ax.set_ylabel('v')
-ax.set_zlabel('E(u,v)')
 
-input("\n--- Pulsar tecla para continuar ---\n")
+def plot_e1(fun):
+	x = np.linspace(-30, 30, 50)
+	y = np.linspace(-30, 30, 50)
+	X, Y = np.meshgrid(x, y)
+	Z = fun([X, Y]) #E_w([X, Y])
+	fig = plt.figure()
+	ax = Axes3D(fig)
+	surf = ax.plot_surface(X, Y, Z, edgecolor='none', rstride=1,
+							cstride=1, cmap='jet')
+	min_point = np.array([w[0],w[1]])
+	min_point_ = min_point[:, np.newaxis]
+	ax.plot(min_point_[0], min_point_[1], fun([min_point_[0], min_point_[1]]), 'r*', markersize=10)
+	ax.set(title='Ejercicio 1.1. Función sobre la que se calcula el descenso de gradiente')
+	ax.set_xlabel('u')
+	ax.set_ylabel('v')
+	ax.set_zlabel('E(u,v)')
 
-#Seguir haciendo el ejercicio...
+	plt.show()
+
+
+print('EJERCICIO SOBRE LA BUSQUEDA ITERATIVA DE OPTIMOS\n')
+print('Ejercicio 1.1\n')
+
+@to_numpy
+def E(u,v):
+    return (u**3 * np.exp(v-2) - 2 * v**2 * np.exp(-u))**2  
+
+#Derivada parcial de E con respecto a u
+def dEu(u,v):
+	return 2 * (u**3 * np.exp(v-2) -2 * v**2 * np.exp(-u)) * (3 * u**2 * np.exp(v-2) + 2 * v**2 * np.exp(-u))
+    
+#Derivada parcial de E con respecto a v
+def dEv(u,v):
+	return 2 * (u**3 * np.exp(v-2) - 2 * v**2 * np.exp(-u)) * (u**3 * np.exp(v-2) - 4 * v * np.exp(-u))
+
+#Gradiente de E
+@to_numpy
+def gradE(u,v):
+    return np.array([dEu(u,v), dEv(u,v)])
+
+def gradient_descent(eta,E,gradE,maxIter,error2get,initial_point):
+	iterations = 0
+	w_t = initial_point
+	all_w = np.array(w_t)
+
+	while iterations < maxIter and E(w_t) > error2get:
+		# All gradient descent in 1 line
+		w_t = w_t - eta*gradE(w_t)
+		all_w = np.append(all_w,w_t)
+		# sum iterations
+		iterations += 1
+
+    
+	return all_w,w_t, iterations
+
+
+eta = 0.1
+maxIter = 10000000000
+error2get = 1e-14
+initial_point = np.array([1.0,1.0])
+_ , w, it = gradient_descent(eta,E,gradE,maxIter,error2get,initial_point)
+
+
+print_output_e1("E(u,v) = (u^3 e^(v-2) - 2v^2 e^(-u))^2",initial_point,eta,it,w)
+plot_e1(E)
+wait()
+
+
+
+@to_numpy
+def f(x,y):
+	return (x + 2)**2 + 2*(y - 2)**2 + 2 * np.sin(2 * np.pi * x) * np.sin(2 * np.pi * y)
+
+def dfx(x,y):
+	return 2 * (x+2) + 4 * np.pi * np.cos(2* np.pi * x) * np.sin(2* np.pi * y)
+
+def dfy(x,y):
+	return 4 * (y-2) + 4 * np.pi * np.sin(2 * np.pi * x) * np.cos(2 * np.pi * y) 
+
+@to_numpy
+def grad_f(x,y):
+    return np.array([dfx(x,y), dfy(x,y)])
+
+
+print("\n")
+print("Ejercicio 1.2.")
+
+eta = 0.01
+maxIter = 50
+error2get = 1e-14
+initial_point = np.array([-1.0,1.0])
+all_w ,w, it = gradient_descent(eta,f,grad_f,maxIter,error2get,initial_point)
+
+print_output_e1("f(x,y) = (x+2)^2 + 2(y-2)^2 + 2 cos(2pi x) cos(2pi y)",initial_point,eta,it,w)
+plot_e1(f)
+wait()
+
+eta = 0.1
+all_w ,w, it = gradient_descent(eta,f,grad_f,maxIter,error2get,initial_point)
+print_output_e1("f(x,y) = (x+2)^2 + 2(y-2)^2 + 2 cos(2pi x) cos(2pi y)",initial_point,eta,it,w)
+plot_e1(f)
+wait()
+
+
+starting_points = np.array([[-0.5,0.5],[1.0,1.0],[2.1,-2.1],[-3.0,3.0],[-2.0,2.0]])
+data = {}
+for s in starting_points:
+	_,w,it = gradient_descent(eta,f,grad_f,maxIter,error2get,s)
+	data[str(s)] = {'(x,y)':w, 'f(w)':f(w),'iterations':it}
+	#print("{} \t - \t {} \t - \t {}".format(s,w,f(w)))
+	
+df = pd.DataFrame.from_dict(data,orient='index')
+
+print(df)
 
 exit()
 
@@ -95,7 +159,7 @@ exit()
 ###############################################################################
 ###############################################################################
 print('EJERCICIO SOBRE REGRESION LINEAL\n')
-print('Ejercicio 1\n')
+print('Ejercicio 2\n')
 
 label5 = 1
 label1 = -1
