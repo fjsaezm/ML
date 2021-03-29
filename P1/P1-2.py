79498137907a20fd96adc81ce4509a05a83a6015
Javier Sáez <fjaviersaezm@gmail.com>
2021-03-29 16:41:48 +0200

    Advances in P1

--- P1/P1-2.py
+++ P1/P1-2.py
@@ -49,26 +49,24 @@ def readData(file_x, file_y):
 	
 	return x, y
 
-
-
-
-
-print('EJERCICIO SOBRE REGRESION LINEAL\n')
-print('Ejercicio 2\n')
-
-
 # Funcion para calcular el error
 def MSE(x,y,w):
-    return  ((x.dot(w) - y)**2).mean(axis = ax)
+	""" Calcula el error cuadrático medio para un modelo lineal"""
+	return (np.linalg.norm(x.dot(w) - y)**2)/len(x)
 
-def dMSE(x,w,y):
-	return 2*(x.T.dot(x.dot(w) -y ))/len(x)
+def dMSE(x,y,w):
+	""" Calcula la derivada del error cuadrático medio para un modelo lineal"""
+	return 2*(x.T.dot(x.dot(w) - y))/len(x)
+
+# Pseudoinverse xT = (x^T x)^-1 x^T 
+def pseudoinverse(x,y):
+	return np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
 
 # Stochastic Gradient Descent
 def sgd(x,y,eta=0.01,max_iterations = 500,batch_size = 32):
 
 	# Initialize w
-	w = np.zeros((x.shape[1],1))
+	w = np.zeros((x.shape[1],))
 	all_w = [w]
 
 	# Create the index for selecting the batches
@@ -82,25 +80,71 @@ def sgd(x,y,eta=0.01,max_iterations = 500,batch_size = 32):
 		current_index_pos += batch_size
 
 		# Update w and all_w
-		w = w - eta*dMSE(x[iteration_index,:], y[iteration_index],w)
-		all_w += [w]
+		w = w - eta*dMSE(x[iteration_index, :], y[iteration_index],w)
+		all_w.append(w)
 
 		# Re-do the index if we have used all the data
-		if current_pos > len(x):
+		if current_index_pos > len(x) or current_index_pos + batch_size > len(x):
 			index = np.random.permutation(np.arange(len(x)))
 			current_index_pos = 0
 
-	return all_w, w
+	return w,all_w
 
+def scatter(x,y = None,w = None,labels = None, title = ""):
+	"""
+	Funcion que permite pintar puntos en el plano
+	- x: datos
+	- y: etiquetas (opcional)
+	- w: vector con pesos de regresion (opcional)
+	- labels: etiquetas para los puntos segun el modelo de regresion(opcional)
+	
+	Se crean dos casos para los modelos de regresión que no sean lineales, se utiliza el caso de 6 características
+	"""
+	# Init subplot
+	_, ax = plt.subplots()
+	ax.set_xlabel('Intensidad Promedio')
+	ax.set_ylabel('Simetría')
+	# Set plot margins
+	xmin, xmax = np.min(x[:, 1]), np.max(x[:, 1])
+	ax.set_xlim(xmin, xmax)
+	ax.set_ylim(np.min(x[:, 2]), np.max(x[:, 2]))
+	# No classes given
+	if y is None:
+		ax.scatter(x[:, 1], x[:, 2])
 
+	# Classes given
+	else:
+		colors = {-1: 'green', 1: 'red'}
+		# For each class
+		for cls, name in [(-1, "Clase -1"), (1, "Clase 1")]:
+      		# Get points of that class
+			class_points = x[y==cls]
+			# Plot them
+			ax.scatter(	class_points[:, 1],
+                		class_points[:, 2],
+                		c = colors[cls],
+                		label = name)
 
+		if w is not None:
+			# Get plot limits
+			x = np.array([xmin, xmax])
 
+			if labels is None:
+				ax.plot(x, -w[1]*x - w[0]/w[2])
+				#for a in w:
+				#	ax.plot(x, (-a[1]*x - a[0])/a[2])
+					
+			else:
+				ax.plot(x, -w[1]*x - w[0]/w[2],label=name)
+				#for a, name in zip(w, labels):
+				#	ax.plot(x, (-a[1]*x - a[0])/a[2], label=name)
+	
+	if y is not None or w is not None:
+		ax.legend()
+  	
+	ax.set_title(title)
+	plt.show() 
 
-   return w
-
-# Pseudoinverse xT = (x^T x)^-1 x^T 
-def pseudoinverse(x,y):
-    return np.matmul(np.matmul(np.linalg.inv(np.matmul(x,x.T)),x.T),y)
 
 
 # Lectura de los datos de entrenamiento
@@ -109,12 +153,34 @@ x, y = readData('datos/X_train.npy', 'datos/y_train.npy')
 x_test, y_test = readData('datos/X_test.npy', 'datos/y_test.npy')
 
 
-w = sgd(?)
-print ('Bondad del resultado para grad. descendente estocastico:\n')
-print ("Ein: ", MSE(x,y,w))
-print ("Eout: ", MSE(x_test, y_test, w))
+print('EJERCICIO SOBRE REGRESION LINEAL\n')
+print('Ejercicio 2\n')
 
-input("\n--- Pulsar tecla para continuar ---\n")
+
+eta = 0.01
+max_iterations = 2000
+batch_size = 32
+
+w,all_w = sgd(x,y,eta,max_iterations,batch_size)
+print ('Bondad del resultado para grad. descendente estocastico en {} iteraciones:\n'.format(max_iterations))
+print ("\tEin: ", MSE(x,y,w))
+print ("\tEout: ", MSE(x_test, y_test, w))
+scatter(x,y,w,title = "Regresión SGD en train")
+scatter(x_test,y_test,w,title = "Regresión SGD en test")
+
+
+wait()
+
+w_pseudo = pseudoinverse(x, y)
+print ('Bondad del resultado para pseudoinversa:')
+print ("\tEin: ", MSE(x,y,w_pseudo))
+print ("\tEout: ", MSE(x_test, y_test, w_pseudo))
+
+scatter(x,y,w,title = "Regresión pseudoinversa en train")
+scatter(x_test,y_test,w,title = "Regresión pseudoinversa en test")
+wait()
+
+exit()
 
 #Seguir haciendo el ejercicio...
 
@@ -129,6 +195,6 @@ def sign(x):
 	return -1
 
 def f(x1, x2):
-	#return sign(?) 
-	return 0
+	return sign((x1-0.2)**2 + x2**2 - 0.6) 
+
 #Seguir haciendo el ejercicio...
