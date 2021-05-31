44be09f84a5ae016b7369564d5bb433e44cbbe0c
Javier Sáez <fjaviersaezm@gmail.com>
2021-05-31 16:06:06 +0200

    Advances

--- P3/classification.py
+++ P3/classification.py
@@ -9,11 +9,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import seaborn as sn
 from sklearn.manifold import TSNE
+from matplotlib import cm
 
 
 np.random.seed(1)
 
 SAVE = True
+SEED = 2022
 
 path_data = "data/clasificacion/Sensorless_drive_diagnosis.txt"
 
@@ -28,18 +30,83 @@ def read_data(path = path_data,test_size = 0.3):
         # Pop removes empty line
         lines = f.read().split('\n')
         lines.pop()
-        print(len(lines))
         for line in lines:
             vec = line.split(" ")
             X.append([float(v) for v in vec[0:len(vec)-1]])
             y.append(int(vec[-1]))
 
-        print(np.array(X).shape)
-        return train_test_split(X,y,test_size = test_size,random_state = 2022)
+        return train_test_split(X,y,test_size = test_size,random_state = SEED)
 
+
+
+def scatter_plot(X, y, axis, ws = None, labels = None, title = None,
+                 figname = "", cmap = 'coolwarm', save_figures = False, img_path = ""):
+    """Muestra un scatter plot de puntos (opcionalmente) etiquetados por clases,
+       eventualmente junto a varias rectas de separación.
+         - X: matriz de características de la forma [x1, x2].
+         - y: vector de etiquetas o clases. Puede ser None.
+         - axis: nombres de los ejes.
+         - ws: lista de vectores 2-dimensionales que representan las rectas
+           (se asumen centradas).
+         - labels: etiquetas de las rectas.
+         - title: título del plot.
+         - figname: nombre para guardar la gráfica en fichero.
+         - cmap: mapa de colores."""
+
+    # Establecemos tamaño, colores e información del plot
+    plt.figure(figsize = (8, 6))
+    plt.xlabel(axis[0])
+    plt.ylabel(axis[1])
+    if title is not None:
+        plt.title(title)
+
+    # Establecemos los límites del plot
+    xmin, xmax = np.min(X[:, 0]), np.max(X[:, 0])
+    ymin, ymax = np.min(X[:, 1]), np.max(X[:, 1])
+    scale_x = (xmax - xmin) * 0.01
+    scale_y = (ymax - ymin) * 0.01
+    plt.xlim(xmin - scale_x, xmax + scale_x)
+    plt.ylim(ymin - scale_y, ymax + scale_y)
+
+    # Mostramos scatter plot con leyenda
+    scatter = plt.scatter(X[:, 0], X[:, 1], c = y, cmap = cmap)
+    if y is not None:
+        legend1 = plt.legend(
+            *scatter.legend_elements(),
+            title = "Clases",
+            loc = "upper right")
+
+    # Pintamos las rectas con leyenda
+    if ws is not None:
+        # Elegimos los mismos colores que para los puntos
+        mask = np.ceil(np.linspace(0, len(cmap.colors) - 1, len(np.unique(y)))).astype(int)
+        colors = np.array(cmap.colors)[mask]
+
+        for w, l, c in zip(ws, labels, colors):
+            x = np.array([xmin - scale_x, xmax + scale_x])
+            plt.plot(x, (-w[0] * x) / w[1], label = l, lw = 2, ls = "--", color = c)
+        plt.legend(loc = "lower right")
+
+    # Añadimos leyenda sobre las clases
+    if y is not None:
+        plt.gca().add_artist(legend1)
+
+    if SAVE:
+        plt.savefig("media/" + figname + ".pdf")
+    
+    plt.show()
+
+    wait()
+
+    
 # Read the data and split in subsets
 X_train,X_test,y_train,y_test =  read_data()
+X_train,X_test,y_train,y_test = np.array(X_train),np.array(X_test),np.array(y_train),np.array(y_test)
+print(X_train.shape)
+pca = PCA(n_components = 30 )
+X_pca = pca.fit_transform(X_train)
+tsne = TSNE(n_components = 2, verbose = 1, perplexity = 500, n_iter = 1000, learning_rate  = 200)
+scatter_plot(tsne.fit_transform(X_pca),y_train,axis = ["x", "y"],title = "Proyección 2-dimensional con TSNE",figname = "tsne")
+#print(X_train)
+#print(y_train)
 
-palette = sn.color_palette("bright", 10)
-X_embedded = TSNE(n_components = 2).fit_transform(X_train)
-sn.scatterplot(X_embedded[:,0],X_embedded[:,1],hue = y,legend = 'full', palette = palette)
--- P3/regression.py
+++ P3/regression.py
@@ -1,55 +1,64 @@
-e
 # Regression
 import pandas as pd
 import numpy as np 
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import seaborn as sn
+from sklearn.pipeline import Pipeline
+from timeit import default_timer
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.linear_model import SGDRegressor, LinearRegression, Ridge
 
 np.random.seed(1)
 
 SAVE = True
+SEED = 2022
 
 path_data = "data/regresion/train.csv"
 
 def wait():
     input("--------------------------------------\n \t \t Presiona Enter para continuar... \n--------------------------------------\n")
 
+# Function to read the data from CSV file. Split in train and test.
 def read_data(path = path_data,test_size = 0.3):
     df = pd.read_csv(path)
     X = df.iloc[:,:-1].to_numpy()
     y = df.iloc[:,-1].to_numpy()
 
-    return train_test_split(X,y,test_size = test_size,random_state = 2022)
-
-
-
-# Read the data and split in subsets
-X_train,X_test,y_train,y_test =  read_data()
-
+    return train_test_split(X,y,test_size = test_size,random_state = SEED)
 
 # Box plot of tags
-green_diamond = dict(markerfacecolor='g', marker='D')
-fig3,ax3 = plt.subplots()
-ax3.boxplot(y_train,vert = False,showmeans = True,flierprops = green_diamond)
-ax3.set_title("Temperaturas en el conjunto de entrenamiento.")
-if SAVE:
-    plt.savefig("media/boxplot_y.pdf")
-plt.show()
-wait()
+def box_plot_values(values,title = " ",save_name = "boxplot_y"):
+    green_diamond = dict(markerfacecolor='g', marker='D')
+    fig3,ax3 = plt.subplots()
+    ax3.boxplot(values,vert = False,showmeans = True,flierprops = green_diamond)
+    ax3.set_title(title)
+    if SAVE:
+        plt.savefig("media/" + save_name + ".pdf")
+    plt.show()
+    wait()
 
-# Average std on datas features
-avg_std = np.mean([X_train[i].std() for i in range(0,X_train.shape[0]) ])
-print("Average Standard deviation of the features of the dataset per row: {}".format(avg_std))
+# Standardize data per columns to a N(0,1)
+def standardize_data(data):
+    Xc = data.copy()
+    Xc = StandardScaler().fit_transform(Xc)
+    return Xc
 
-# Data preprocessing
+# Apply standardization + PCA to data
+def preprocess_data(data):
+    Xp = standardize_data(data)
+    pca = PCA(n_components = 0.95)
+    pca.fit(Xp)
+    Xp = pca.transform(Xp)
+    
+    return Xp,pca
 
 
 
-# Correlation Matrix
+# Correlation Matrix of a set of data
 def corr_matrix(data,name_fig):
     df = pd.DataFrame(data,columns = np.arange(data.shape[1]))
     corr_m = df.corr().abs()
@@ -64,13 +73,38 @@ def corr_matrix(data,name_fig):
     plt.show()
     wait()
 
+
+
+# -------------------------------------------------------------------
+# ------------ IMPLEMENTATION BEGINS HERE ---------------------------
+# -------------------------------------------------------------------
+
+
+# Read the data and split in subsets
+X_train,X_test,y_train,y_test =  read_data()
+
+# Find if there are columns with low std
+df = pd.DataFrame(X_train,columns = np.arange(X_train.shape[1]))
+print("Printing columns which variance is smaller than 0.05...")
+for col in df.columns:
+    if df.var()[col] <= 0.05:
+        print(col)
+print("Done")
+
+
+# Box plot Y values in train
+#box_plot_values(y_train,title = "Temperaturas en el conjunto de entrenamiento.")
+
+# Average std on datas features
+avg_std = np.mean([X_train[i].std() for i in range(0,X_train.shape[0]) ])
+print("Average Standard deviation of the features of the dataset per row: {}".format(avg_std))
+
+# Data preprocessing
 print("Preprocessing...")
 
-def standardize_data(data):
-    Xc = data.copy()
-    Xc = StandardScaler().fit_transform(Xc)
-    return Xc
 
+
+# Plot standardized data's correlation matrix
 Xp = standardize_data(X_train)
 #corr_matrix(Xp,"corr-normalized")
 
@@ -78,22 +112,69 @@ Xp = standardize_data(X_train)
 dfp = pd.DataFrame(Xp,columns = np.arange(Xp.shape[1]))
 corr_m = dfp.corr().abs()
 # Select upper triangle of correlation matrix
-upper = corr_m.where(np.triu(np.ones(corr_m.shape), k=1).astype(np.bool))
+upper = corr_m.where(np.triu(np.ones(corr_m.shape), k=1).astype(bool))
 # Find index of feature columns with correlation greater than 0.95
-to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+to_drop = [column for column in upper.columns if any(upper[column] >= 0.95)]
 
+
+X_train_rem = X_train.copy()
+X_train_rem = np.delete(X_train_rem, np.array(to_drop), axis = 1)
 print("There are {} variables which correlation with another is greater than 0.95".format(len(to_drop)))
 
 
-def preprocess_data(data):
-    Xp = standardize_data(data)
-    pca = PCA(n_components = 0.95)
-    pca.fit(Xp)
-    Xp = pca.transform(Xp)
+#Pipelines
+#from sklearn.ensemble import IsolationForest
+#clf = IsolationForest(random_state=SEED).fit(X_train)
+#arr = clf.predict(X_train)
+
+#X_train = X_train[np.where(arr == 1)]
+#y_train = y_train[np.where(arr == 1)]
+
+preprocess = [
+    #("pre-standardize", StandardScaler()),
+    #("PCA", PCA(n_components = 0.95)),
+    ("standardize",StandardScaler())
+]
+preprocess_pipeline = Pipeline(preprocess)
+
+X_train_pre = preprocess_pipeline.fit_transform(X_train)
+
+#corr_matrix(X_train_pre,"corr-pca")
+
+# By using LinearRegression(), we can use later any linear regression
+model_pipe = Pipeline(preprocess + [("reg",LinearRegression())])
+
+search_space = [
+    {"reg": [SGDRegressor(penalty = 'l2',
+                          random_state = SEED)],
+     'reg__alpha' : [1/10.0**i for i in range(1,5)],
+     'reg__learning_rate':['constant','optimal','adaptive'],
+     'reg__max_iter':[2000,5000,10000],
+    },
+    {'reg': [Ridge()],
+     'reg__alpha': [1/10.0**i for i in range(1,5)],
+     'reg__max_iter': [2000,5000,10000]
     
-    return Xp,pca
+    }
 
-X_t_pca,transformer = preprocess_data(X_train)
-print("Data's shape after PCA: {}".format(X_t_pca.shape))
+]
 
-corr_matrix(X_t_pca,"corr-pca")
+print("Realizando la búsqueda en el espacio dado de modelos lineales...", flush = True)
+start = default_timer()
+best_reg = GridSearchCV(model_pipe, search_space, scoring = "neg_mean_squared_error", cv = 5, n_jobs = -1)
+best_reg.fit(X_train_rem,y_train)
+end = default_timer() - start
+
+print("Terminado en {}.".format(end))
+
+print(" ----- Mejor regresor lineal encontrado ------")
+print(" - Parámetros:")
+print(best_reg.best_params_['reg'])
+print(" - Error en Cross Validation")
+print(-best_reg.best_score_)
+print("----------------------------------------------")
+print("Grid scores")
+means = best_reg.cv_results_['mean_test_score']
+stds = best_reg.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, best_reg.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
